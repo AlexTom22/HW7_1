10875c2e5a2225e525d7f78863f142e8f000986b
Alexander Tomelo <qa.tomelo.an@gmail.com>
2022-10-25 13:35:40 +0300

    upload my project fils

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture(scope='function')
+def browser():
+    print('\nstart browser...')
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.implicitly_wait(5)
+    yield browser
+    print('\nquit browser...')
+    browser.quit()
